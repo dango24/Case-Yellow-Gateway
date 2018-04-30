e753c22e2e6ff387a337624fb2954a271916148e
Dan <dangolan24@gmail.com>
2018-04-30 23:07:27 +0300

    clean ups

--- src/main/java/com/icarusrises/caseyellowgateway/domain/webSite/model/SpeedTestWebSite.java
+++ src/main/java/com/icarusrises/caseyellowgateway/domain/webSite/model/SpeedTestWebSite.java
@@ -1,10 +1,17 @@
 package com.icarusrises.caseyellowgateway.domain.webSite.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by Dan on 12/10/2016.
  */
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SpeedTestWebSite {
 
@@ -16,101 +23,4 @@ public class SpeedTestWebSite {
     private long startMeasuringTimestamp;
     private double downloadRateInMbps; // Mega bit per second
     private double downloadRateInKBps; // Mega bit per second
-
-    public SpeedTestWebSite() {
-    }
-
-    public SpeedTestWebSite(String speedTestIdentifier) {
-        this.speedTestIdentifier = speedTestIdentifier;
-        this.succeed = true;
-    }
-
-    public SpeedTestWebSite(boolean succeed) {
-        this.succeed = succeed;
-    }
-
-    public SpeedTestWebSite(String urlAddress, String speedTestIdentifier, long startMeasuringTimestamp) {
-        this(false, urlAddress, speedTestIdentifier, startMeasuringTimestamp);
-    }
-
-    public SpeedTestWebSite(boolean succeed, String urlAddress, String speedTestIdentifier, long startMeasuringTimestamp) {
-        this.succeed = succeed;
-        this.urlAddress = urlAddress;
-        this.speedTestIdentifier = speedTestIdentifier;
-        this.startMeasuringTimestamp = startMeasuringTimestamp;
-    }
-
-    public String getUrlAddress() {
-        return urlAddress;
-    }
-
-    public void setUrlAddress(String urlAddress) {
-        this.urlAddress = urlAddress;
-    }
-
-    public String getSpeedTestIdentifier() {
-        return speedTestIdentifier;
-    }
-
-    public void setSpeedTestIdentifier(String speedTestIdentifier) {
-        this.speedTestIdentifier = speedTestIdentifier;
-    }
-
-    public long getStartMeasuringTimestamp() {
-        return startMeasuringTimestamp;
-    }
-
-    public void setStartMeasuringTimestamp(long startMeasuringTimestamp) {
-        this.startMeasuringTimestamp = startMeasuringTimestamp;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public boolean isSucceed() {
-        return succeed;
-    }
-
-    public void setSucceed(boolean succeed) {
-        this.succeed = succeed;
-    }
-
-    public String getNonFlashResult() {
-        return nonFlashResult;
-    }
-
-    public void setNonFlashResult(String nonFlashResult) {
-        this.nonFlashResult = nonFlashResult;
-    }
-
-    public Double getDownloadRateInMbps() {
-        return downloadRateInMbps;
-    }
-
-    public void setDownloadRateInMbps(Double downloadRateInMbps) {
-        this.downloadRateInMbps = downloadRateInMbps;
-    }
-
-    public Double getDownloadRateInKBps() {
-        return downloadRateInKBps;
-    }
-
-    public void setDownloadRateInKBps(Double downloadRateInKBps) {
-        this.downloadRateInKBps = downloadRateInKBps;
-    }
-
-    @Override
-    public String toString() {
-        return "SpeedTestWebSite{" +
-                "succeed=" + succeed +
-                ", urlAddress='" + urlAddress + '\'' +
-                ", speedTestIdentifier='" + speedTestIdentifier + '\'' +
-                ", startMeasuringTimestamp=" + startMeasuringTimestamp +
-                '}';
-    }
 }
