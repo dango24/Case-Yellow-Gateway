a5e7b2367107e345eaec9399106d4e567bb8a43e
dangolan <dangolan@amazon.com>
2018-09-19 14:11:47 +0300

    Change gateway health check payload to 'Justice will be served'

--- src/main/java/com/icarusrises/caseyellowgateway/controllers/GatewayController.java
+++ src/main/java/com/icarusrises/caseyellowgateway/controllers/GatewayController.java
@@ -31,7 +31,7 @@ public class GatewayController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/health")
     public String health() {
-        return "we are all good over here";
+        return "Justice will be served";
     }
 
     @PutMapping("/add-user")
